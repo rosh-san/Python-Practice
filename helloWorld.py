1d6ec2fd658c5e25079379ec29fc33d989c8b182
Roshan <roshangaikwad2003@gmail.com>
2026-06-13 02:58:48 +0530

    Error Handling

--- helloWorld.py
+++ helloWorld.py
@@ -1,29 +1,40 @@
-print("Hello, World!")  
+print("HELLO WORLD!!!")  
 
 # Take input from the user 
 a = input("Variable a: ")
 b = input("Variable b: ")
-#multiple inputs at once
-# a, b = input("Variable a and b: ").split()
-
 operation = input("Operation (+, -, *, /, %, **, //): ")
 
 #perform calculations
-if operation == "+":
-    c = int(a) + int(b)
-elif operation == "-":
-    c = int(a) - int(b)
-elif operation == "*":
-    c = int(a) * int(b)
-elif operation == "/":
-    c = int(a) / int(b)
-elif operation == "%":
-    c = int(a) % int(b)
-elif operation == "**":
-    c = int(a) ** int(b)
-elif operation == "//":
-    c = int(a) // int(b)
-else: c = "Invalid operation"
+#Error-Handling with try-except block
+try:
+    #input to int conversion
+    num_a=int(a)
+    num_b=int(b)
 
-# print("Result:", c)
-print(f"Result: {c}")
+    if operation == "+":
+        c = num_a + num_b
+    elif operation == "-":
+        c = num_a - num_b
+    elif operation == "*":
+        c = num_a * num_b
+    elif operation == "/":
+        c = num_a / num_b
+    elif operation == "%":
+        c = num_a % num_b
+    elif operation == "**":
+        c = num_a ** num_b
+    elif operation == "//":
+        c = num_a // num_b
+    else: 
+        c = "Invalid operation"
+
+    # print("Result:", c)
+    print(f"Result: {c:.2f}")  # Format the result to 2 decimal places
+
+except ValueError:
+    print("Invalid input. Please enter integer values for a and b.")
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+except Exception as e:
+    print(f"An error occurred: {e}")
---
+++ pyThon.py
@@ -0,0 +1,12 @@
+print("Hello World !!!")
+a=20
+b=30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+print(a==b)
+print(a!=b)
