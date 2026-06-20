2d3d7e3522c83cd5182838954cc3684c7cec7401
Roshan <roshangaikwad2003@gmail.com>
2026-06-20 23:54:33 +0530

    Completed profile trio by adding the standard User class

--- OOP-practice/app.py
+++ OOP-practice/app.py
@@ -1,4 +1,4 @@
-from profiles import NGO, Admin
+from profiles import User, NGO, Admin
 from system import EcoLoopPlatform
 
 print("--- Welcome to the Eco-Loop Backend Console ---")
@@ -6,14 +6,18 @@ print("--- Welcome to the Eco-Loop Backend Console ---")
 # Initialize the EcoLoopPlatform, booting the central database
 server = EcoLoopPlatform()
 
+# Creating a User
+everyday_citizen = User("Alice", "alice@example.com", "555-0999", "123 Service Road, Chembur")
 # Creating a NGO
 green_earth = NGO("Green Earth Foundation", "contact@greenearth.org", "555-0199", "NGO-1042")
 # Creating an Admin
 system_admin = Admin("Roshan", "admin@ecoloop.com", "555-0000", 5)
 
+server.register_account(everyday_citizen)   
 server.register_account(green_earth)
 server.register_account(system_admin)
 
 server.view_database()
 
+server.database["alice@example.com"].request_pickup("Two broken laptops and a monitor")
 server.database["contact@greenearth.org"].accept_pickup()
--- OOP-practice/profiles.py
+++ OOP-practice/profiles.py
@@ -1,5 +1,13 @@
 from base_account import Account
 
+class User(Account):
+    def __init__(self, name: str, email: str, phone: str, address: str):
+        super().__init__(name, email, phone)
+        self.address = address
+
+    def request_pickup(self, item_description: str) -> None:
+        print(f"[User Action]: {self.name} requested a pickup for '{item_description}' at {self.address}.")
+
 # The NGO Blueprint
 class NGO(Account):
     def __init__(self, name: str, email: str, phone: str, reg_id: str):
