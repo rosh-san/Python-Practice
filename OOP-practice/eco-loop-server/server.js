99ddf00eb657d173d52ff37b12b8a0f1915d7f10
Roshan <roshangaikwad2003@gmail.com>
2026-06-23 23:59:50 +0530

    Playing with GET POST PATCH

--- OOP-practice/eco-loop-server/server.js
+++ OOP-practice/eco-loop-server/server.js
@@ -1,5 +1,5 @@
+const crypto = require('crypto');
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
@@ -25,11 +25,18 @@ app.get('/api/system-status', (req, res) => {
 });
 
 app.post('/api/request-pickup', (req, res) => {
-    activePickup.push(req.body);
+    const enrichedData = {
+        ticketId: crypto.randomUUID(),
+        status: "Pending",
+        requestDetails: req.body
+    };
+
+    activePickup.push(enrichedData);
+    
     res.json({
         message: "Pickup scheduled.",
         activePickups: activePickup,
-        receivedData: req.body
+        receivedData: enrichedData
     });
 });
 
@@ -41,4 +48,20 @@ app.get('/api/view-pickups', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`[System]: Eco-Loop server is currently running on http://localhost:${PORT}`);
+});
+
+app.patch('/api/accept-pickup/:id', (req, res) => {
+    const pickupId = req.params.id;
+    const ticket = activePickup.find(t => t.ticketId === req.params.id);
+    if (ticket) {
+        ticket.status = "Accepted by NGO";
+        res.json({
+            message: `Pickup with ID ${pickupId} has been accepted.`,
+            updatedTicket: ticket
+        });
+    } else {
+        res.status(404).json({
+            message: `Pickup with ID ${pickupId} not found.`
+        });
+    }
 });
